295060b84d837acff1ebfe585628c1a4cb104083
Maciej Gawłowski <gal0wsky@macius-MBP.local>
2022-12-17 15:45:38 +0100

    Bledy z index.html

--- projektr/addEarn.php
+++
@@ -1,9 +0,0 @@
-<?php
- $db = mysqli_connect("localhost","root","","sheepurhack");
-
- $ile = $_POST['ile'];
- $nazwa = $_POST['nazwa'];
-
- mysqli_query($db,"INSERT INTO `earn`(ile, nazwa) VALUES ('$ile','$nazwa');");
- header("Location: index.php");
-?>
--- projektr/index.js
+++
@@ -1,6 +0,0 @@
-function add(a)
-{
-    a.parentElement.children[0].classList.toggle("invisible");
-    a.parentElement.children[1].classList.toggle("invisible");
-    a.parentElement.children[2].classList.toggle("invisible");
-}
