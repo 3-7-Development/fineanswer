14ecb7fb4d79068a47204bcfdc83fb63f868332e
jajszu <bartekjaje@gmail.com>
2022-12-17 14:28:33 +0100

    chyba to wszystko ide poline i taboret

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
     </div>
     <div id="earn">
-        <h1>Przychód</h1>
+        <h1>TWÓJ PRZYCHÓD</h1>
         <div id="earnA">
             <div >
                 <?php
@@ -22,81 +22,60 @@
                 ?>
             </div>
         <div>
-        <p class="button" onclick="add(this)">Dodaj</p>
-        <div class="add invisible">
-        <form method="POST" action="addEarn.php"> <input type="number" name="ile"><input name="nazwa" type="text"><input type="submit"></form> 
-        </div>
-        <p class="button invisible" onclick="add(this)">Anuluj</p>
+        
         </div>
        
         
         </div>
 
     </div>
-    
     <div id="pay">
-
+        <h1>TWOJE WYDATKI</h1>
        <div class="category">
-            <h1>Rachunki</h1>
+            <h2>Rachunki</h2>
             <?php
                 include "pay.php";
 
                 display("rachunki");
             ?>
              <div>
-        <p class="button" onclick="add(this)">Dodaj</p>
-        <div class="add invisible">
-        <form method="POST" action="addPay.php"><input type="hidden" name="kategoria" value="rachunki"> <input type="number" name="ile"><input name="nazwa" type="text"><input type="submit"></form> 
-        </div>
-        <p class="button invisible" onclick="add(this)">Anuluj</p>
+       
         </div>
         </div>
 
         <div class="category">
-            <h1>Jedzenie</h1>
+            <h2>Jedzenie</h2>
             <?php
             
 
                 display("jedzenie");
             ?>
              <div>
-        <p class="button" onclick="add(this)">Dodaj</p>
-        <div class="add invisible">
-        <form method="POST" action="addPay.php"><input type="hidden" name="kategoria" value="jedzenie"> <input type="number" name="ile"><input name="nazwa" type="text"><input type="submit"></form> 
-        </div>
-        <p class="button invisible" onclick="add(this)">Anuluj</p>
+       
         </div>
         </div>
 
         <div class="category">
-            <h1>Rozrywka</h1>
+            <h2>Rozrywka</h2>
             <?php
         
 
                 display("rozrywka");
             ?>
              <div>
-        <p class="button" onclick="add(this)">Dodaj</p>
-        <div class="add invisible">
-        <form method="POST" action="addPay.php"><input type="hidden" name="kategoria" value="rozrywka"> <input type="number" name="ile"><input name="nazwa" type="text"><input type="submit"></form> 
-        </div>
-        <p class="button invisible" onclick="add(this)">Anuluj</p>
+       
         </div>
         </div>
 
         <div class="category">
-            <h1>Inne</h1>
+            <h2>Inne</h2>
             <?php
          
 
                 display("inne");
             ?>
              <div>
-        <p class="button" onclick="add(this)">Dodaj</p>
-        <div class="add invisible">
-        <form method="POST" action="addPay.php"><input type="hidden" name="kategoria" value="inne"> <input type="number" name="ile"><input name="nazwa" type="text"><input type="submit"></form> 
-        </div>
-        <p class="button invisible" onclick="add(this)">Anuluj</p>
+        
         </div>
         </div>
     </div>
@@ -104,10 +83,7 @@
    
     <div id="mid">
         <div id="save">
-            <form action="" method="POST">
-            <h2>Dodaj Długi do spłacenia </h2>
-            <input type="number" name="wartosc"><input type="text" name="nazwa"><input type="date" name="data"><button type="submit" name="dodaj">dodaj</button>
-            </form>
+           
 
             <?php
                 if(isset($_POST['dodaj'])){
@@ -126,7 +102,7 @@
             ?>
 
 
-            <p></p>
+          
         </div>
         
         
@@ -137,39 +113,63 @@
                
 
                 $q = mysqli_query($db,"SELECT * FROM dlugi");
-                
+                $suma = 0;
                     for($i = 0; $i < mysqli_num_rows($q); $i++)
                     {
                         $tab = mysqli_fetch_array($q);
+                        $suma+=$tab["rata"];
 
                         ?>
-                                <div class="debt_display">
-                                <div class="display_percentage">
-                                           <?php echo $tab['ilosc_splacona']."zł / ".$tab['wartosc']."zł";?>
-                                    </div>
-                                    
-                                    <div class="display_title">
-                                    <h3><?php echo $tab['nazwa']; ?></h3>
-                                    <br>
-                                    <h5>Deadline: <?php echo $tab['deadline']; ?></h5>
-                                    </div>
-                                    <div style="width: 
-                                    
-                                    <?php 
-                                        $a = $tab['wartosc'];
-                                        $b = $tab['ilosc_splacona'];
-                                        $percent = $b/$a * 750;
-                                        echo($percent);
-                                    ?>px" class="progress_bar">
-                                        
-                                    </div>
+                                <div class="title">
+                                                                <h4><?php echo $tab["nazwa"] ?></h4>
+                                <div><?php echo round($tab["ilosc_splacona"]/$tab["wartosc"]*100);?>% </div>
+                                <div>
+                            <div  class="debt_box">
+                                <div style="width:<?php echo round($tab["ilosc_splacona"]/$tab["wartosc"]*100);?>%" class="progress"></div>  </div>
 
-                                    
-                                </div>
+                          
+
                                 <?php
-                    }
+                            }
 
-        ?>
+               
+                            $db = mysqli_connect("139.162.177.109", "syh37DevUser", "Th!s!sB3stP4ssw0rd", "sheepurhack");
+
+                            $pay = 0;
+                            $res = mysqli_query($db,"SELECT * FROM wydatki ");
+                               while($a = mysqli_fetch_row($res))
+                               {
+                                    $pay+=$a[2];
+                               }
+                               $earn = 0;
+                               $res = mysqli_query($db,"SELECT * FROM zyski");
+                                  while($a = mysqli_fetch_row($res))
+                                  {
+                                       $earn+=$a[2];
+                             
+                                  }
+
+        ?>      <div class="summary">
+           <div class="graph" style="position:relative;right:300px">
+
+            <div style="width:<?php echo round($pay/$earn*100);?>%;height:50px;background-color:red;"><?php echo round($pay/$earn*100);?>% - OPŁATY</div>
+            <div style="width:<?php echo round($suma/$earn*100);?>%;height:50px;background-color:orange;"><?php echo round($suma/$earn*100);?>% - SPŁATA</div>
+            <div style="width:<?php echo round(($earn-$suma-$pay)/$earn*100);?>%;height:50px;background-color:green;"><?php echo round(($earn-$suma-$pay)/$earn*100);?>% - OSZCZĘDNOŚĆ</div>
+             </div>
+             <div class="right">
+             <p>
+                Wydałeś <?php echo $pay; ?> PLN ze swojego budżetu.
+                                </p>
+             <p>
+                W tym miesiącu potrzebujesz mieć <?php echo $suma; ?> PLN na spłatę rat.
+                                </p>
+                                <p>
+                <?php if($earn-$pay>0) echo "Udało ci się zaoszczędzić ".$earn-$pay-$suma." PLN."; else echo "Wydałeś za dużo o ".$pay+$suma-$earn." PLN musisz zredukować koszty niektórych twoich wydatków" ?>
+                                </p>
+                                </div>         
+                
+                    </div>
+ 
 
 </body>
 </html>
--- pay.php
+++ pay.php
@@ -7,7 +7,7 @@ function display($a)
  $res = mysqli_query($db,"SELECT * FROM wydatki WHERE kategoria LIKE '$a'");
     while($a = mysqli_fetch_row($res))
     {
-        echo "<div class='payments'><h3>".$a[0]."</h3><div>".$a[1]."</div></div>";
+        echo "<div class='payments'><h3>".$a[1]."</h3><div><p>".$a[2]."zł</p></div></div>";
     }
 }
 ?>
